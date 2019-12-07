1a1242c8a704d0c9c6f0671662c7c795215ad0cc
Thomas Rolfe <thomasrolfe@yahoo.co.uk>
2019-12-07 22:25:17 +0000

    Laravel from scratch part 7

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -10,12 +10,13 @@
 <body>
 
 
-
-    <ul>
-        @foreach($tasks as $task)
-            <li>{{ $task->body }}</li>
-        @endforeach
-    </ul>
+<ul>
+    @foreach($tasks as $task)
+        <li>
+            <a href="/tasks/{{ $task->id }}">{{ $task->body }}</a>
+        </li>
+    @endforeach
+</ul>
 
 </body>
 </html>
