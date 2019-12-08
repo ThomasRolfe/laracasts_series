fde3d0f887511ba3d0c608560fd69653541be24c
Thomas Rolfe <thomasrolfe@yahoo.co.uk>
2019-12-08 22:34:48 +0000

    Laravel from scratch part 11

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('content')
+
+    showing a posts
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,5 @@
 */
 
 
-Route::get('/tasks', 'TasksController@index');
-
-Route::get('/tasks/{task}', 'TasksController@show');
-
+Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
