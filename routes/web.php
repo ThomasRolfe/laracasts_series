3e64b350c4aef99aa9e871d624779301a8c3c76b
Thomas Rolfe <thomasrolfe@yahoo.co.uk>
2019-12-15 21:04:43 +0000

    Start of part 18, added auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/posts/create', 'PostsController@create');
 Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts', 'PostsController@store');
 Route::post('/posts/{post}/comments', 'CommentsController@store');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
